ef035733bfb4eeedf7a173fd0e7a0ba0ccfab673
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2015-01-08 14:04:56 -0500

    Added basic bracket method.

--- package-dir/R/multifile-accessors.R
+++ package-dir/R/multifile-accessors.R
@@ -108,4 +108,26 @@ stan_commander <- setRefClass(Class="stan_commander",
 		}
 	)
 )
-		
+	
+## Below works if j is just one number... now generalize... :/
+setMethod(
+	f="[",
+	signature = signature(x='stan_commander', i='character', j="numeric"),
+	definition = function(x, i, j) {
+		o <- do.call(what=x$get_parameter, args=list(i, j))
+		return(o)
+	}
+)
+
+#setMethod(
+#	f="[<-",
+#	signature = signature(x='block_distribution', i='character',j='character'),
+#	definition = function(x, i, j, value) {
+#		x$write(x=value, from=i, to=j)
+#	return(x)
+#}
+#)
+#
+
+
+
