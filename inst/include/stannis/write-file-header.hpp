8ef47a610523e50a9cf54a87aff167f2b6f20059
Krzysztof Sakrejda <sakrejda@umass.edu>
2018-08-16 15:55:10 -0400

    Updating overal rewrite function.

--- inst/include/stannis/write-file-header.hpp
+++ inst/include/stannis/write-file-header.hpp
@@ -3,73 +3,75 @@
 
 #include <cstdint>
 #include <iostream>
+#include <boost/uuids/uuid.hpp>
 
 namespace stannis {
   namespace stantastic {
-  const char* magic = "Stantastic!";
-  const std::uint_least32_t file_version = 1;
-  const std::uint_least16_t major_stan = 2;
-  const std::uint_least16_t minor_stan = 17;
-  const std::uint_least16_t patch_stan = 3;
 
-  constexpr std::uint_least8_t n_iterations_position = std::ios::beg + 11 
-    + sizeof(std::uint_least32_t) + sizeof(std::uint_least16_t) * 3;
+    const char* magic = "Stantastic!";
+    const std::uint_least32_t file_version = 1;
+    const std::uint_least16_t major_stan = 2;
+    const std::uint_least16_t minor_stan = 17;
+    const std::uint_least16_t patch_stan = 3;
+  
+    constexpr std::uint_least8_t n_iterations_position = std::ios::beg + 11 
+      + sizeof(std::uint_least32_t) + sizeof(std::uint_least16_t) * 3;
+    }
+  
+    template<class S>
+    void write_stantastic_header(
+      S & stream, 
+      const boost::uuids::uuid & tag
+    ) {
+      // Magic string          
+      stream.write((char*)(&magic), 11);       
+      
+      // File version
+      stream.write((char*)(&file_version), sizeof(file_version));
+      stream.write((char*)(&major_stan), sizeof(major_stan));
+      stream.write((char*)(&minor_stan), sizeof(minor_stan));
+      stream.write((char*)(&patch_stan), sizeof(patch_stan));
+  
+      // UUID tag
+      stream.write((char*)(&tag.data[0]), tag.static_size());
+      
+      return;
+    }
+  
+    template <class S>
+    void insert_iterations(
+      S & stream,
+      std::uint_least32_t n_iterations
+    ) {
+      std::streampos current_position = stream.tellp();
+      stream.seekp(stantastic::n_iterations_position);
+      stream.write((char*)(&n_iterations), sizeof(n_iterations));
+      stream.seekp(current_position);
+    };
+  
+    template <class S>
+    void write_description(
+      S & stream,
+      std::uint_least32_t n_parameters
+    ) {
+      std::uint_least32_t n_iterations = 0; // modified later
+      stream.write((char*)(&n_iterations), sizeof(n_iterations));
+      stream.write((char*)(&n_parameters), sizeof(n_parameters));
+      return;
+    }
+  
+    template <class S>
+    void write_comment(
+      S & stream,
+      std::string & comment
+    ) {
+      std::uint_least32_t:q
+  	    L = comment.length();
+      stream.write((char*)(&L), sizeof(L));
+      stream.write((char*)(&comment[0]), L);
+      return;
+    };
   }
-
-  template<class S>
-  void write_stantastic_header(
-    S & stream, 
-    const boost::uuids::uuid & tag
-  ) {
-    // Magic string          
-    stream.write((char*)(&magic), 11);       
-    
-    // File version
-    stream.write((char*)(&file_version), sizeof(file_version));
-    stream.write((char*)(&major_stan), sizeof(major_stan));
-    stream.write((char*)(&minor_stan), sizeof(minor_stan));
-    stream.write((char*)(&patch_stan), sizeof(patch_stan));
-
-    // UUID tag
-    stream.write((char*)(&tag.data[0]), tag.static_size());
-    
-    return;
-  }
-
-  template <class S>
-  void insert_iterations(
-    S & stream,
-    std::uint_least32_t n_iterations
-  ) {
-    std::streampos current_position = stream.tellp();
-    stream.seekp(stantastic::n_iterations_position);
-    stream.write((char*)(&n_iterations), sizeof(n_iterations));
-    stream.seekp(current_position);
-  };
-
-  template <class S>
-  void write_description(
-    S & stream,
-    std::uint_least32_t n_parameters
-  ) {
-    std::uint_least32_t n_iterations = 0; // modified later
-    stream.write((char*)(&n_iterations), sizeof(n_iterations));
-    stream.write((char*)(&n_parameters), sizeof(n_parameters));
-    return;
-  }
-
-  template <class S>
-  void write_comment(
-    S & stream,
-    std::string & comment
-  ) {
-    std::uint_least32_t:q
-	    L = comment.length();
-    stream.write((char*)(&L), sizeof(L));
-    stream.write((char*)(&comment[0]), L);
-    return;
-  };
-
 } // 
 
 
--- src/reader.cpp
+++ src/reader.cpp
@@ -1,124 +1,69 @@
 
 #include <iostream>
 #include <fstream>
-#include <sstream>
-#include <iomanip>
 
-#include <tuple>
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <numeric>
+#include <cstdint>
 
-#include <stannis/types.hpp>
+#include <boost/filesystem.hpp>
+
 #include <stannis/file-helpers.hpp>
-#include <stannis/sample-header.hpp>
 #include <stannis/reader.hpp>
+#include <stannis/write-file-header.hpp>
+#include <stannis/sample-header.hpp>
 
 namespace stannis {
 
-  void update_dimensions(std::string::iterator head, std::string::iterator end, std::vector<int>& dim) {
-    int i = 0;
-    std::string::iterator tail;;
-    for (head = std::find(head, end, '.'); head != end; head = std::find(head + 1, end, '.')) {
-      tail = std::find(head + 1, end, '.');
-      dim[i++] = std::stoi(std::string(head + 1, tail));
-    }
-  }
-  
-  bool read_parameters(std::string& line, const header_t& h, parameter_t& p) {
-    std::stringstream data_stream(line);
-  
-    int n_parameters = std::get<1>(h);
-    std::vector<std::vector<int>> index = std::get<5>(h);
-    p.resize(n_parameters);
-   
-    std::vector<double> x = read_csv_vector(line);
-    if (x.size() < n_parameters)
-      return false;
-    for (unsigned int i = 0; i < index.size(); ++i) {
-      for (unsigned int j = 0; j < index[i].size(); j++) {
-        p[i].push_back(x[index[i][j]]);  
-      }
-    }
-    return true;
-  }
-  
-  void reshape_parameters(const header_t& h, parameter_t& p) {
-    int n_parameters = p.size();
-    std::vector<std::vector<int>> dimensions = std::get<4>(h);
-    for (dv_size_t i = 0; i < p.size(); ++i) {
-      dv_size_t n_entries = std::accumulate(
-        dimensions[i].begin(), dimensions[i].end(), 1, std::multiplies<dv_size_t>());
-      dv_size_t n_iterations = p[i].size() / n_entries;
-      std::vector<double> x(p[i].size());
-      for (dv_size_t j = 0; j < x.size(); ++j) {
-        dv_size_t k = j % n_iterations;
-        dv_size_t m = j / n_iterations;
-        dv_size_t n = k * n_entries + m;  
-        x[j] = p[i][n];
-      }
-      p[i] = x;
-    }  
-  }
-  
-  mm_t read_mass_matrix(std::ifstream& f) {
-    std::string line;
-    std::getline(f, line);
-    auto head = std::find(line.begin(), line.end(), '=') + 1;
-    auto tail = line.end();
-    double step_size = std::stod(std::string(head, tail));
-  
-    std::getline(f, line);
-    std::getline(f, line);
-    std::vector<double> mm = read_csv_vector(line.substr(1));
-    return std::make_tuple(step_size, mm);
-  }
-  
-  timing_t read_timing(std::ifstream& f) {
-    std::string line;
-    timing_t tt;
-    std::getline(f, line);
-    auto head = std::find(line.begin() + 1, line.end(), ':') + 1;
-    auto tail = std::find(head, line.end(), 's');
-    tt.push_back(std::stod(std::string(head, tail)));
-    std::getline(f, line);
-    head = line.begin() + 1;
-    tail = std::find(head, line.end(), 's');
-    tt.push_back(std::stod(std::string(head, tail)));
-    std::getline(f, line);
-    head = line.begin() + 1;
-    tail = std::find(head, line.end(), 's');
-    tt.push_back(std::stod(std::string(head, tail)));
-    return tt;
-  }
-    
-  std::tuple<header_t, parameter_t, mm_t, timing_t> 
-  read_samples(
-    std::ifstream& f
+  bool rewrite(
+    const boost::filesystem::path & source,
+    const boost::filesystem::path & root,
+    const std::string & name,
+    const boost::uuids::uuid & tag,
+    const std::uint_least32_t n_parameters;
+    const std::string & comment
   ) {
-    header_t header;
-    parameter_t parameters;
-    mm_t mm;
-    timing_t tt;
-  
+    std::uintmax_t source_size = boost::filesystem::file_size(source);
+    boost::filesystem::fstream source_stream;
+    source_stream.open(source);
+
+    // Shared storage directory
+    boost::filesystem::path storage_path = root /= name;
+    boost::filesystem::path header_path = storage_path /= "header.bin";
+    boost::filesystem::path names_path = storage_path /= "names.bin";
+    boost::filesystem::path dim_path = storage_path /= "dimensions.bin";
+    boost::filesystem::path storage_path = create_storage_file(root, name, 2 * source_size);
+
+    // Name file
+    boost::filesystem::fstream os;
+    os.open(header_path);
+    write_stantastic_header(os, tag);
+    write_description(os, n_parameters);
+    write_comment(os, comment);
+    os.close()
+
+    boost::filesystem::fstream name_stream(names_path);
+    boost::filesystem::fstream dim_stream(dim_path);
     std::string line;
-    bool got_header = false;
-    while (std::getline(f, line)) {
-      if (!got_header && !is_comment(line)) {
-        header = read_header(line);
-        got_header = true;
-      } else if (got_header && !is_comment(line)) {
-        read_parameters(line, header, parameters);
-      } else if (is_mm_start(line)) {
-        mm = read_mass_matrix(f);  // advances past line
-      } else if (got_header && is_comment(line)) {
-        tt = read_timing(f); // advances past line
+    bool complete = false;
+    while (std::getline(source_stream, line)) {
+      if (!is_comment(line)) {
+        complete = rewrite_header(line, name_stream, dim_stream);  // FIXME: streams
         break;
       }
     }
-    reshape_parameters(header, parameters);
-    return std::make_tuple(header, parameters, mm, tt);
+    if (!complete)
+      return false;
+
+    boost::iostreams::mapped_file_sink storage(storage_path); 
+    //boost::iostreams::stream<boost::iostreams::mapped_file_sink> os(storage);
+    rewrite_parameters(source_stream, names, dims, path);
+    rewrite_mass_matrix(source_stream); 
+    rewrite_parameters(source_stream, names, dims, path);
+    rewrite_timing(f);
+    return true;
   }
   
 }
