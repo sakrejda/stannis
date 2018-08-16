4358fbea1dccabd27e02451e068d9a33ced5475d
Krzysztof Sakrejda <sakrejda@umass.edu>
2018-08-16 10:48:27 -0400

    This is more or less what it would look like if I worked with a basic_streambuf (but then I'd have to derive to access protected funcitons) so with other streams this is double-buffering.

--- inst/include/stannis/sample-header.hpp
+++ inst/include/stannis/sample-header.hpp
@@ -10,29 +10,65 @@
 
 namespace stannis {
 
-  void read_name(
-    std::basic_istream & stream,
+  /* Read a name from a stream and copy it to a string. 
+   *
+   * - expects to only be called if there are more names to read,
+   *   that is this is the first read or the last read ended on
+   *   a ',' character.
+   *
+   * @tparam S a stream type with the 'read' method
+   * @param stream a stream of type S
+   * @param buffer used for buffered reads from the stream
+   * @param head iterator into the buffer to start read from
+   * @param tail iterator into the buffer to end read at
+   * @return true if there are more names to read
+   */
+
+  template <class S>
+  int read_name(
+    S & stream,
     std::array<char> & buffer,
     std::array<char>::iterator & head,
     std::array<char>::iterator & tail
     std::string & name
   ) {
+    int offset = 0;
+    if (buffer.size() == 0) 
+      throw std::logic_error("Read buffer size must be greater than zero.");
     tail = head;
     while (true) {
       tail++;
-      if (*tail == ',' || *tail == '\n') {
-	name.resize(tail - head);
-        std::copy(head, tail, name);
+      if (tail != buffer.end() && (*tail == ',' || *tail == '\n' || *tail == '.')) {
+	name.resize(offset + (tail - head));
+        std::copy(head, tail, name.begin());
+	head = tail;
 	break;
       }
       if (tail == buffer.end()) {
-        offset = tail - head;
+        offset += tail - head;
 	name.resize(offset);
-	std::copy(head, tail, name);
+	std::copy(head, tail, name.begin());
 	stream.read(&buffer[0], buffer.size());
+	if (stream.gcount() == 0)
+	  throw std::logic_error("Unfinished name and unreadable stream.");
+	head = buffer.begin();
+	tail = head;
+	if (*tail == ',' || *tail = '\n' || *tail == '.')
+	  break;
       }
     }
-    return;
+    return (*head != '\n');
+  }
+
+  template <class S>
+  int read_dims(
+    S & stream,
+    std::array<char> & buffer,
+    std::array<char>::iterator & head,
+    std::array<char>::iterator & tail
+    std::string & name
+  ) {
+
   }
 
   /* Streaming re-write of text header line to output file. */
@@ -44,46 +80,28 @@ namespace stannis {
     int buffer_size = 8192
   ) {
     std::array<char> buffer(buffer_size);
-    stream.read(&buffer[0], buffer_size);
-    int paved = buffer_size;
-    int offset = 0;
-    bool end_header = false;
-  
     std::array<char>::iterator head = buffer.begin();
-    std::array<char>::iterator tail = std::find(head, head + paved, ',');
-    if (tail == head + paved) 
-      throw std::logic_error("First name is too long.");
-    std::array<char>::iterator end = std::find(head, tail, '\n');
-    if (end != tail) {
-      n_extra = end - tail - 1; // -1 for implicitely dropping the newline!
-      tail = end;
-      while (n_extra != 0) {
-        stream.putback(*(++end));  // pre-increment skips newline
-	--n_extra;
-      }
-      end_header = true;
+    std::array<char>::iterator tail = buffer.begin();
+    std::string previous_name;
+    std::streampos previous_pos;
+    std::string current_name;
+
+    bool read_more = read_name(stream, buffer, head, tail, previous_name);
+    previous_pos = stream.tellg() - (buffer.size() - head);
+
+    if (!read_more) {
+      name_stream.write((char*)(&previous_name[0]), previous_name.length());
+      std::uint_least16_t d = 1;
+      dim_stream.write((char*)(&d), sizeof(d));
+      dim_stream.write((char*)(&d), sizeof(d));
+      return;
     }
-    std::array<char>::iterator dot = std::find(head, tail, '.');
+    if (*head == '.') {
+      read_more = read_dims(stream, buffer, ++head, ++tail, d);
+    }
 
-    std::string previous_name(head, tail);
-    std::array<char>::iterator previous_dot;
-    std::array<char>::iterator previous_tail;
     while (stream.good()) {
-      head = tail + 1;
-      tail = std::find(head, head + paved, ',');
-      end = std::find(head, tail, '\n');
-      if (end != tail) {
-        n_extra = end - tail - 1; // -1 for implicitely dropping the newline!
-        tail = end;
-        while (n_extra != 0) {
-          stream.putback(*(++end));  // pre-increment skips newline
-  	--n_extra;
-        }
-        end_header = true;
-      }
-      dot = std::find(head, tail, '.');
-
-
+      read_more = read_name(stream, head, tail, previous_name);
       if (current_name == previous_name) {
 	previous_dot = dot;
 	previous_tail = tail;
