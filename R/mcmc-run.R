0cf0701abc665bce431b101416983582ce78467c
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2018-09-03 18:09:37 -0400

    'runs good'

--- R/mcmc-run.R
+++ R/mcmc-run.R
@@ -16,6 +16,8 @@ read_run = function(root='.', uuid = NULL, control_file = 'finalized.yaml',
     run_root = file.path(root, uuid)
   else
     run_root = root
+
+  uuid = as_uuid(run_root)
    
   sample_file_path = file.path(run_root, sample_file)
   if (!file.exists(sample_file_path))
@@ -30,25 +32,27 @@ read_run = function(root='.', uuid = NULL, control_file = 'finalized.yaml',
     stop("Control file is missing.")
 
   sample_root = file.path(run_root, 'sample')
+  if (!dir.exists(sample_root))
+    dir.create(sample_root)
+
   diagnostic_root = file.path(run_root, 'diagnostic')
+  if (!dir.exists(diagnostic_root))
+    dir.create(diagnostic_root)
+
+  comment = as.character(Sys.time())
 
   stannis::rewrite_stan_csv(source = sample_file_path, 
     root = sample_root,
-    tag = uuid, comment = Sys.time())
+    tag = uuid, comment = comment)
   stannis::rewrite_stan_csv(source = diagnostic_file_path, 
     root = diagnostic_root,
-    tag = uuid, comment = Sys.time())
+    tag = uuid, comment = comment)
   metadata = yaml::yaml.load_file(control_file_path)
-  sample_dimensions = stannis::get_dimensions(
-    file.path(sample_root, 'dimensions.bin'),
-    file.path(sample_root, 'names.bin')
-  )
-  diagnostic_dimensions = stannis::get_dimensions(
-    file.path(diagnostic_root, 'dimensions.bin'),
-    file.path(diagnostic_root, 'names.bin')
-  )
+  sample_dimensions = stannis::get_dimensions(sample_root)
+  diagnostic_dimensions = stannis::get_dimensions(diagnostic_root)
  
   return(list(
+    uuid = uuid,
     run_root = run_root,
     sample_root = sample_root,
     sample_dimensions = sample_dimensions,
--- src/rewrite-interface.cpp
+++ src/rewrite-interface.cpp
@@ -10,32 +10,43 @@
 
 #include <boost/filesystem.hpp>
 #include <boost/uuid/uuid.hpp>
+#include <boost/uuid/name_generator.hpp>
 #include <boost/uuid/uuid_io.hpp>
 
 #include <string>
 #include <fstream>
 
 RcppExport SEXP hash_to_uuid(SEXP hash_) {
+  BEGIN_RCPP
   std::vector<std::string> hashes = Rcpp::as<std::vector<std::string>>(hash_);
   std::string tag_string;
   for (auto & s : hashes) 
     tag_string.append(s);
-  boost::uuids::uuid tag = boost::uuids::name_generator_sha1(tag_string);
-  return Rcpp::wrap(tag);
+  boost::uuids::name_generator_sha1 gen(boost::uuids::ns::oid());
+  boost::uuids::uuid tag = gen(tag_string);
+  std::string t = boost::uuids::to_string(tag);
+  return Rcpp::CharacterVector(t);
+  END_RCPP
 }
 
 RcppExport SEXP rewrite_stan_csv(SEXP source_, SEXP root_, SEXP tag_, SEXP comment_
 ) {
+  BEGIN_RCPP
   boost::filesystem::path source = Rcpp::as<boost::filesystem::path>(source_);
   boost::filesystem::path root = Rcpp::as<boost::filesystem::path>(root_);
   boost::uuids::uuid tag = Rcpp::as<boost::uuids::uuid>(tag_);
-  std::string comment = Rcpp::as<std::string>(comment_);
-  bool complete = stannis::rewrite_stan_csv(source, root, tag, comment);
+  std::vector<std::string> comment = Rcpp::as<std::vector<std::string>>(comment_);
+  std::string comment_string;
+  for (auto & s : comment) 
+    comment_string.append(s);
+  bool complete = stannis::rewrite_stan_csv(source, root, tag, comment_string);
   Rcpp::RObject result = Rcpp::wrap(complete);
   return result;
+  END_RCPP
 }
 
 RcppExport SEXP get_dimensions(SEXP dim_path_, SEXP name_path_) {
+  BEGIN_RCPP
   boost::filesystem::path dim_path = Rcpp::as<boost::filesystem::path>(dim_path_);
   boost::filesystem::path name_path = Rcpp::as<boost::filesystem::path>(name_path_);
   std::vector<std::vector<uint>> dims = stannis::get_dimensions(dim_path);
@@ -44,9 +55,11 @@ RcppExport SEXP get_dimensions(SEXP dim_path_, SEXP name_path_) {
   for (int i = 0; i < names.size(); ++i)
     result.push_back(dims[i], names[i]);
   return result;
+  END_RCPP
 }
 
 RcppExport SEXP get_parameter(SEXP root_, SEXP name_) {
+  BEGIN_RCPP
   std::string name = Rcpp::as<std::vector<std::string>>(name_)[0];
   boost::filesystem::path root = Rcpp::as<boost::filesystem::path>(root_);
   boost::filesystem::path dim_path(root);
@@ -57,9 +70,11 @@ RcppExport SEXP get_parameter(SEXP root_, SEXP name_) {
     Rcpp::Named("data") = draws,
     Rcpp::Named("dims") = dims
   );
+  END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
+    {"hash_to_uuid", (DL_FUNC) &hash_to_uuid, 1},
     {"rewrite_stan_csv", (DL_FUNC) &rewrite_stan_csv, 4},
     {"get_dimensions", (DL_FUNC) &get_dimensions, 2},
     {"get_parameter", (DL_FUNC) &get_parameter, 2},
